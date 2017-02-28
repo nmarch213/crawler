c871a620eaa54819d015a2de8cb85627321bc698
FBM-User <FBM-User>
2017-02-28 15:18:30 -0600

    'init'

---
+++ index.js
@@ -0,0 +1,57 @@
+var request = require('request');
+var cheerio = require('cheerio');
+const parser = require('xml-parser');
+var URL = require('url-parse');
+var GoogleSpreadsheet = require('google-spreadsheet');
+const async = require('async');
+
+const crawler = require('./xmlcrawler/crawler');
+
+var doc = new GoogleSpreadsheet('1NfEezqXJ_P43MiLuFEEj9BMka-yn2e-jeWpr7QV4uK0');
+var sheet;
+
+
+async.series([
+  function setAuth(step){
+    var creds = require('./google-gen-creds.json');
+    doc.useServiceAccountAuth(creds,step);
+  },
+
+  function getInfoAndWorksheets(step) {
+    doc.getInfo(function(err, info) {
+      console.log('Loaded doc: '+info.title+' by '+info.author.email);
+      sheet = info.worksheets[1];
+      console.log('sheet 1: ' + sheet);
+      step();
+    });
+  }
+
+]);
+var sitemapURLs = [];
+sitemapURLs = crawler.crawlXmlSitemap('http://www.jbheatingandair.com/');
+
+console.log(sitemapURLs[0]);
+// for (var i = 0; i < sitemapURLs.length; i++) {
+//   console.log('running');
+//   request(sitemapURLs[i], function(error, response,body){
+//     if(err){
+//       console.log("Error: " + error);
+//     }
+//
+//     // Check status code (200 is HTTP OK)
+//     console.log("Status code: " + response.statusCode);
+//     if(response.statusCode === 200) {
+//       // Parse the document body
+//       var $ = cheerio.load(body);
+//       var keywords = $('strong').text();
+//       googleSheet.site.push({
+//         "keywords": keywords,
+//         "url" : i
+//       });
+//
+//       console.log("these are the keywords: " + $('strong').text());
+//
+//     }
+//   });
+//
+// }
---
+++ xmlcrawler/crawler.js
@@ -0,0 +1,54 @@
+const parser = require('xml-parser');
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
+
+module.exports = {
+
+  crawlXmlSitemap: function(websiteRoot){
+
+    var sitemapURLs = [];
+
+    //This is the website XML sitemap you will be crawling
+    var sitemapUrl = websiteRoot + "/sitemap.xml";
+    console.log("Currently Crawling: " + sitemapUrl);
+    request(sitemapUrl, function(error, response, body) {
+       if(error) {
+         console.log("Error: " + error);
+         return;
+       }
+       // Check status code (200 is HTTP OK)
+       console.log("Status code: " + response.statusCode);
+       if(response.statusCode === 200) {
+         // Parse the document body
+         var $ = cheerio.load(body,{
+           xmlmode: true
+         });
+         //Parses the website and puts it into a string
+         parsedXmlString = $(body).text();
+
+         //This creates an array of individual strings that are from the xml file
+         var arrayOfParsedXml = parsedXmlString.split(" ");
+         var urls = [];
+
+         //regular expression to find a URl
+         var expression = '(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})';
+         var regex = new RegExp(expression);
+
+         //Loops through the array of strings that were split, and looks to
+         //find the URL's using the REGEX above.
+         for (var i = 0; i < arrayOfParsedXml.length; i++) {
+           //Loop through the array of strings lookign to see if there is a URl
+           if(arrayOfParsedXml[i].match(regex)){
+             //If a URl is found, add it to the final URLS list
+             urls.push(arrayOfParsedXml[i]);
+           };
+         }
+         return urls;
+       }
+    });
+
+  }
+
+
+}
