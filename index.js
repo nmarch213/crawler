4448673b8bf440f60684168c6d8ac1ba0177eee7
Nicholas March <n.march213@gmail.com>
2017-03-15 14:38:23 -0500

    Setting up the basic express app with handlebars

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+var express = require('express');
+
+
+var app = express();
+
+//Routes
+var indexRoutes = require('./routes/index.js');
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars')
+  .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
 const googleRunner = require('./xmlcrawler/googleSpreadsheet');
 
-googleRunner.addContentToGoogleSpreadsheet('http://www.jbheatingandair.com/');
+//googleRunner.addContentToGoogleSpreadsheet('http://www.jbheatingandair.com/');
+
+//index routes
+app.use(indexRoutes);
+
+app.listen(app.get('port'), function(){
+  console.log( 'SEOCrawl Live' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var router = require('express').Router();
+
+//homepage
+router.get('/', function(req, res) {
+      res.render('index');
+})
+
+module.exports = router;
